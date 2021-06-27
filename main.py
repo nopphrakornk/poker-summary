1a00d64e01b02df88e49c744ba19745f29d658fa
nk2673 <nk2673@nyu.edu>
2021-06-27 13:57:03 +0700

    pokers

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class PokerSession():
         self.players_list.append(new_player)
 
     def buyin(self, buyer, seller, amount):
-        self.money_grid[self.players_list.index(buyer), self.players_list.index(seller)] += amount
+        self.money_grid[self.players_list.index(buyer.capitalize()), self.players_list.index(seller.capitalize())] += amount
 
     def getSummary(self):
         lines = list()
@@ -28,19 +28,88 @@ class PokerSession():
                         lines.append('{loser} owe {winner}: {amount}'.format(loser = players[col], winner =  players[row], amount = int(np.abs(lose_minus_win))))
         return lines
 
-players = ['Mac', 'Ter', 'Nop']
+players = ['Mac', 'Ter', 'Nop', 'Bart']
 
 sess = PokerSession(players)
-sess.buyin('Ter', 'Nop', 50)
-sess.buyin('Mac', 'Ter', 100)
-sess.buyin('Mac', 'Ter', 50)
-sess.buyin('Nop', 'Ter', 50)
-sess.buyin('Mac', 'Ter', 50)
-sess.buyin('Nop', 'Ter', 50)
-sess.buyin('Nop', 'Mac', 50)
-sess.buyin('Mac', 'Nop', 50)
-sess.buyin('Ter', 'Mac', 50)
-sess.buyin('Ter', 'Nop', 50)
-sess.buyin('Mac', 'Nop', 7.5)
 
-print(sess.getSummary())
+
+sess.buyin("Bart","ter", 50)
+sess.buyin("Nop","ter", 50)
+sess.buyin("Ter","mac", 25)
+sess.buyin("Ter","nop", 25)
+sess.buyin("Ter","mac", 25)
+sess.buyin("BaRt","nop", 25)
+sess.buyin("Bart","ter", 25)
+sess.buyin("Ter","mac", 50)
+sess.buyin("Nop","ter", 50)
+sess.buyin("Nop","ter", 50)
+sess.buyin("Ter","bart", 50)
+sess.buyin("Nop","ter", 50)
+sess.buyin("Nop","mac", 50)
+sess.buyin("Ter","mac", 50)
+sess.buyin("Ter","nop", 25)
+sess.buyin("Ter","bart", 25)
+sess.buyin("mac","nop", 25)
+sess.buyin("mac","ter", 25)
+sess.buyin("Ter","nop", 50)
+sess.buyin("Bart","mac", 25)
+sess.buyin("Bart","nop", 25)
+sess.buyin("Bart","nop", 50)
+sess.buyin("mac","ter", 50)
+sess.buyin("bart","nop", 50)
+sess.buyin("Ter","mac", 50)
+sess.buyin("Nop","mac", 50)
+sess.buyin("Nop","ter", 50)
+sess.buyin("Bart","mac", 50)
+sess.buyin("Nop","bart", 50)
+sess.buyin("ter","mac", 50)
+sess.buyin("Bart","ter", 50)
+sess.buyin("Nop","mac", 50)
+sess.buyin("Nop","mac", 25)
+sess.buyin("Bart","ter", 50)
+sess.buyin("Bart","ter", 50)
+sess.buyin("Nop","bart", 25)
+sess.buyin("Nop","ter", 25)
+sess.buyin("Ter","bart", 50)
+sess.buyin("Ter","bart", 50)
+sess.buyin("Nop","bart", 25)
+sess.buyin("Nop","mac", 25)
+sess.buyin("Ter","mac", 50)
+sess.buyin("Mac","nop", 25)
+sess.buyin("Mac","ter", 25)
+sess.buyin("ter","bart", 50)
+sess.buyin("Nop","bart", 50)
+sess.buyin("Bart","mac", 50)
+sess.buyin("Ter","nop", 25)
+sess.buyin("Ter","mac", 25)
+sess.buyin("Bart","mac", 50)
+sess.buyin("Ter","mac", 7.5)
+sess.buyin("Nop","ter", 32.25)
+sess.buyin("Nop", "mac", 50)
+sess.buyin("bart", "mac", 50)
+sess.buyin("ter", "mac", 50)
+
+#pokdeng starts here
+sess.buyin("ter", "mac", 100)
+sess.buyin("bart", "mac", 100)
+sess.buyin("nop", "mac", 100)
+
+
+sess.buyin("mac", "bart", 50)
+sess.buyin("nop", "bart", 50)
+sess.buyin("ter", "bart", 50)
+
+sess.buyin("mac", "bart", 50)
+sess.buyin("nop", "bart", 50)
+sess.buyin("ter", "bart", 50)
+
+sess.buyin("mac", "bart", 50)
+sess.buyin("nop", "bart", 50)
+sess.buyin("ter", "bart", 50)
+
+
+sess.buyin("nop", "Ter", 100)
+sess.buyin("bart", "Ter", 100)
+sess.buyin("mac", "Ter", 100)
+
+sess.getSummary()
