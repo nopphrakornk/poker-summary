0a585398472c19fe17b91052cc9159977b1f4683
nk2673 <nk2673@nyu.edu>
2021-08-09 00:35:57 +0700

    leaving copy paste from line Macos

--- main.py
+++ main.py
@@ -28,88 +28,94 @@ class PokerSession():
                         lines.append('{loser} owe {winner}: {amount}'.format(loser = players[col], winner =  players[row], amount = int(np.abs(lose_minus_win))))
         return lines
 
-players = ['Mac', 'Ter', 'Nop', 'Bart']
+players = ['Bart', 'Ter', 'Nop', 'Mac']
 
 sess = PokerSession(players)
-
-
-sess.buyin("Bart","ter", 50)
-sess.buyin("Nop","ter", 50)
-sess.buyin("Ter","mac", 25)
-sess.buyin("Ter","nop", 25)
-sess.buyin("Ter","mac", 25)
-sess.buyin("BaRt","nop", 25)
-sess.buyin("Bart","ter", 25)
-sess.buyin("Ter","mac", 50)
-sess.buyin("Nop","ter", 50)
-sess.buyin("Nop","ter", 50)
-sess.buyin("Ter","bart", 50)
-sess.buyin("Nop","ter", 50)
-sess.buyin("Nop","mac", 50)
-sess.buyin("Ter","mac", 50)
-sess.buyin("Ter","nop", 25)
-sess.buyin("Ter","bart", 25)
-sess.buyin("mac","nop", 25)
-sess.buyin("mac","ter", 25)
-sess.buyin("Ter","nop", 50)
-sess.buyin("Bart","mac", 25)
-sess.buyin("Bart","nop", 25)
-sess.buyin("Bart","nop", 50)
-sess.buyin("mac","ter", 50)
-sess.buyin("bart","nop", 50)
-sess.buyin("Ter","mac", 50)
-sess.buyin("Nop","mac", 50)
-sess.buyin("Nop","ter", 50)
-sess.buyin("Bart","mac", 50)
-sess.buyin("Nop","bart", 50)
-sess.buyin("ter","mac", 50)
-sess.buyin("Bart","ter", 50)
-sess.buyin("Nop","mac", 50)
-sess.buyin("Nop","mac", 25)
-sess.buyin("Bart","ter", 50)
-sess.buyin("Bart","ter", 50)
-sess.buyin("Nop","bart", 25)
-sess.buyin("Nop","ter", 25)
-sess.buyin("Ter","bart", 50)
-sess.buyin("Ter","bart", 50)
-sess.buyin("Nop","bart", 25)
-sess.buyin("Nop","mac", 25)
-sess.buyin("Ter","mac", 50)
-sess.buyin("Mac","nop", 25)
-sess.buyin("Mac","ter", 25)
-sess.buyin("ter","bart", 50)
-sess.buyin("Nop","bart", 50)
-sess.buyin("Bart","mac", 50)
-sess.buyin("Ter","nop", 25)
-sess.buyin("Ter","mac", 25)
-sess.buyin("Bart","mac", 50)
-sess.buyin("Ter","mac", 7.5)
-sess.buyin("Nop","ter", 32.25)
-sess.buyin("Nop", "mac", 50)
+sess.buyin("ter", "mac", 50)
+sess.buyin("mac", "bart", 50)
+sess.buyin("ter", "mac", 50)
+sess.buyin("mac", "bart", 50)
+sess.buyin("ter", "bart", 35)
+sess.buyin("ter", "mac", 15)
+sess.buyin("bart", "mac", 50)
+sess.buyin("mac", "bart", 50)
+sess.buyin("ter", "bart", 50)
+sess.buyin("ter", "bart", 50)
+sess.buyin("ter", "bart", 50)
+sess.buyin("ter", "bart", 50)
+sess.buyin("ter", "bart", 50)
+sess.buyin("mac", "ter", 50)
+sess.buyin("bart", "ter", 39)
+sess.buyin("bart", "mac", 21)
+sess.buyin("bart", "mac", 50)
+sess.buyin("bart", "mac", 30)
+sess.buyin("bart", "ter", 10)
+sess.buyin("ter", "mac", 50)
+sess.buyin("mac", "bart", 50)
+sess.buyin("bart", "mac", 50)
+sess.buyin("bart", "mac", 45)
+sess.buyin("bart", "ter", 5)
 sess.buyin("bart", "mac", 50)
 sess.buyin("ter", "mac", 50)
-
-#pokdeng starts here
-sess.buyin("ter", "mac", 100)
-sess.buyin("bart", "mac", 100)
-sess.buyin("nop", "mac", 100)
-
-
-sess.buyin("mac", "bart", 50)
-sess.buyin("nop", "bart", 50)
-sess.buyin("ter", "bart", 50)
-
-sess.buyin("mac", "bart", 50)
-sess.buyin("nop", "bart", 50)
-sess.buyin("ter", "bart", 50)
-
-sess.buyin("mac", "bart", 50)
-sess.buyin("nop", "bart", 50)
-sess.buyin("ter", "bart", 50)
-
-
-sess.buyin("nop", "Ter", 100)
-sess.buyin("bart", "Ter", 100)
-sess.buyin("mac", "Ter", 100)
+sess.buyin("bart", "mac", 50)
+sess.buyin("ter", "mac", 5)
+sess.buyin("bart", "mac", 15)
+sess.buyin("bart", "ter", 50)
+sess.buyin("mac", "ter", 50)
 
 sess.getSummary()
+
+22:10 Nop 黃俊城 ter tid nop 25
+22:10 Nop 黃俊城 ter tid bart 20
+22:10 Nop 黃俊城 ter tid mac 5
+22:12 Nop 黃俊城 nop tid bart 50
+22:14 Nop 黃俊城 mac tid ter 35
+22:14 Nop 黃俊城 mac tid bart 15
+22:22 Nop 黃俊城 nop tid ter 50
+22:23 Nop 黃俊城 bart tid ter 30
+22:23 Nop 黃俊城 bart tid mac 20
+22:25 Nop 黃俊城 bart tid ter 45
+22:25 Nop 黃俊城 bart tid mac 5
+22:27 Nop 黃俊城 mac tid bart 50
+22:32 Nop 黃俊城 ter tid bart 50
+22:33 Nop 黃俊城 nop tid ter 50
+22:38 Nop 黃俊城 bart tid ter 50
+22:38 Nop 黃俊城 mac tid ter 50
+22:40 Nop 黃俊城 bart tid ter 50
+22:44 Nop 黃俊城 nop tid bart 45
+22:44 Nop 黃俊城 nop tid mac 5
+22:46 Nop 黃俊城 bart tid ter 30
+22:47 Nop 黃俊城 barti tid mac 20
+22:49 Nop 黃俊城 mac tid ter 40
+22:49 Nop 黃俊城 mac tid bart 10
+22:50 Nop 黃俊城 bart tid ter 50
+22:51 Nop 黃俊城 nop tid bart 50
+22:53 Ter Photos
+22:53 Ter Lag kanard kid wa gu yung yu nai call
+22:54 Ter Lol
+22:56 Nop 黃俊城 bart tid ter 50
+23:03 Nop 黃俊城 nop tid mac 50
+23:04 Nop 黃俊城 ter tid bart 50
+23:05 Nop 黃俊城 nop tid bart 50
+23:08 Nop 黃俊城 nop tid bart 50
+23:10 Nop 黃俊城 mac tid bart 50
+23:11 Nop 黃俊城 mac tid ter 50
+23:24 Nop 黃俊城 bart tid nop 50
+23:25 Nop 黃俊城 ter tid nop 30
+23:25 Nop 黃俊城 ter tid bart 10
+23:25 Nop 黃俊城 ter tid mac 10
+23:28 Ter Bart?
+23:29 Bart phone overgeat
+23:29 Bart dai la
+23:37 Nop 黃俊城 mac tid bart 50
+23:37 Nop 黃俊城 nop tid bart 50
+23:38 Nop 黃俊城 ter tid bart 50
+23:39 Bart Photos
+23:39 Bart gu tong use nee puer hai phone yen long rew
+23:40 Nop 黃俊城 https://developer.mozilla.org/en-US/docs/Web/API/Blob
+23:43 Nop 黃俊城 ['Nop owe Mac: 55',
+'Mac owe Ter: 160',
+'Nop owe Ter: 45',
+'Bart owe Ter: 125',
+'Mac owe Bart: 130',
+'Nop owe Bart: 245']
